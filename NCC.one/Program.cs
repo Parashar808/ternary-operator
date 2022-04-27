2eb2a07f2208457ae5e78a97126c0f188742b9ae
Parashar <baralpara4@gmail.com>
2022-04-27 08:09:17 +0545

    ternary operator

---
+++ NCC.one/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApp
+{
+    public class PRogram{
+    public static void Main(){
+       Console.WriteLine("i am a boy .");
+       test();
+        return ;
+
+    }
+    public static void test(){
+        Console.WriteLine("enter 1 ,2 or 3");
+          int inputUser = Convert.ToInt32(Console.ReadLine());
+          string result = (inputUser == 1)? "ONE": (inputUser==2)? "two":(inputUser==3)? "three" : "invalid";
+        Console.WriteLine(result);
+
+}
+    
+
+    }
+
+}
